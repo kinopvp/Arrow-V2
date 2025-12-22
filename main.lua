6349c1e66f19e36bfd5b2d7bb413b5b8d928fe9a
Kino <89075262+kinopvp@users.noreply.github.com>
2025-12-22 07:17:15 +0100

    Fix indentation for muscle legend loading

--- main.lua
+++ main.lua
@@ -4,7 +4,8 @@ if PlaceId == 855499080 then
     -- skywars
     loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/skywars.lua"))()
 elseif PlaceId == 3623096087 then
-   -- muscle legend    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/musclelegends.lua"))()
+   -- muscle legend 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/musclelegends.lua"))()
 else
     warn("not supportedd.")
 end
