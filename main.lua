2bfff9f1955c61204b60a2d1d035c73d494e8822
Kino <89075262+kinopvp@users.noreply.github.com>
2025-12-22 03:19:55 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 855499080 then
+    -- skywars
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/main.lua"))()
+elseif PlaceId == 3623096087 then
+   -- muscle legend    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/musclelegends.lua"))()
+else
+    warn("not supportedd.")
+end
