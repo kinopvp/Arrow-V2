7d5beb02f6dcd94c1c1b1a4ab1058cc825508f13
Kino <89075262+kinopvp@users.noreply.github.com>
2025-12-22 03:31:15 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local PlaceId = game.PlaceId
 
 if PlaceId == 855499080 then
     -- skywars
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/skywars.lua"))()
 elseif PlaceId == 3623096087 then
    -- muscle legend    loadstring(game:HttpGet("https://raw.githubusercontent.com/kinopvp/Arrow-V2/refs/heads/main/musclelegends.lua"))()
 else
